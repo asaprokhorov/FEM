0668cf6241c70c9c014cb84ae86495926e483bec
sanyokprokhor <oleksandrprohr@gmail.com>
2016-03-18 10:37:37 +0200

    created ui

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from lsm import *
+import sys
+from PyQt5.QtWidgets import *
 
 
 
@@ -21,32 +23,31 @@ states = []
 
 solution = h_adaptive_LSM(func, nodes, create_basis(nodes), accuracy, states)
 
-xs = []
-ys = []
-size = 100
-h = (b - a) / size
-for i in range(size + 1):
-    x = a + h * i
-    xs.append(x)
-    ys.append(solution(x))
-plt.plot(xs, ys)
-plt.show()
 
-#
-# import sys
-# from PyQt5.QtWidgets import *
-# from PyQt5.QtCore import *
-#
-# if __name__ == '__main__':
-#
-#     app = QApplication(sys.argv)
-#
-#     listView = QTableWidget()
-#     listView.setRowCount(len(states))
-#     listView.setColumnCount(2)
-#     for i in range(len(states)):
-#         listView.setItem(i, 0, QTableWidgetItem(states[i].size.__str__()))
-#         listView.setItem(i, 1, QTableWidgetItem(states[i].error.__str__()))
-#     listView.doubleClicked.connect(draw)
-#     listView.show()
-#     sys.exit(app.exec_())
+def draw(row):
+    xs = []
+    ys = []
+    size = 100
+    h = (b - a) / size
+    for i in range(size + 1):
+        x = a + h * i
+        xs.append(x)
+        ys.append(states[row.row()].function(x))
+    plt.plot(xs, ys)
+    plt.show()
+
+
+# ui pyqt
+app = QApplication(sys.argv)
+
+listView = QTableWidget()
+listView.setRowCount(len(states))
+listView.setColumnCount(2)
+listView.setHorizontalHeaderItem(0, QTableWidgetItem("Size"))
+listView.setHorizontalHeaderItem(1, QTableWidgetItem("Error"))
+for i in range(len(states)):
+    listView.setItem(i, 0, QTableWidgetItem("{0}".format(states[i].size)))
+    listView.setItem(i, 1, QTableWidgetItem("{0:.5}".format(states[i].error)))
+listView.doubleClicked.connect(draw)
+listView.show()
+sys.exit(app.exec_())
